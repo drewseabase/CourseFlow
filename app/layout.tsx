b2f8e83a4f0784513869417c0d7632d14d7df281
drewseabase <143631824+drewseabase@users.noreply.github.com>
2026-02-08 16:40:24 -0500

    got the navBar basically done and working

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,17 +16,8 @@ export default function RootLayout({
   return(
     <html lang="en">
       <body>
-        {/*Animated Background Blobs*/}
-        <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
-          <div className="absolute w-[400px] h-[400px] rounded-full blur-[80px] opacity-30 bg-gradient-1
-          -top-[100px] -left-[100px] animate-float"/>
-            <div className="absolute w-[350px] h-[350px] rounded-full blur-[80px] opacity-30 bg-gradient-3
-            -bottom-[100px] -right-[100px] animate-float [animation-delay: -5s]"/>
-            <div className="absolute w-[300px] h-[300px] rounded-full blur-[80px] opacity-30
-            bg-gradient-4 top-[50%] right-[20%] animate-float [animation-delay:-10s]"/>
-        </div>
         {/*Main Content*/}
-        <div className="max-w-[1500[px] mx-auto px-6 py-6">
+        <div className="max-w-375 mx-auto px-10 py-10">
           <Navbar/>
           <main>{children}</main>
         </div>
